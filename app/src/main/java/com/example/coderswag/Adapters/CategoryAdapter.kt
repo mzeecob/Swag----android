bcca16ffbd113e577d5624b463eb1ffe3b933e97
Regis <reshimwe2025@gmailcom>
2019-10-17 17:11:21 +0200

    using base adapter to be able to pass our custom layout to use it

---
+++ app/src/main/java/com/example/coderswag/Adapters/CategoryAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.coderswag.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.example.coderswag.Model.Category
+import com.example.coderswag.R
+
+class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter(){
+
+
+    val context = context
+    val categories = categories
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val categoryView: View
+
+        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+
+
+        return categoryView
+    }
+
+    override fun getItem(position: Int): Any {
+        return categories[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return categories.count()
+
+    }
+
+}
